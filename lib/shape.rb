45367cefc1c906b1430063911490352b5f78c6a9
Andrew Newman <andrewfnewman@gmail.com>
2013-08-19 13:58:45 +1000

    Merge bounding boxes.

--- lib/shape.rb
+++ lib/shape.rb
@@ -2,4 +2,8 @@ class Shape < Struct.new(:point_array, :bounding_box)
   def self.sort_by_bounding_box(shapes)
     shapes.sort_by { |s| s.bounding_box.top_left }
   end
+
+  def self.merged_bound_box(shapes)
+    BoundingBox.minimum_bounding_rectangle(shapes.map { |s| s.bounding_box })
+  end
 end
